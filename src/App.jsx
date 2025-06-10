8a01cee54a2e4961b17be4f2420e84625debd989
mjwang <wang.michellej@gmail.com>
2025-06-10 11:25:46 -0700

    implement useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
   const [pokemonData, setPokemonData] = useState(null)
+  let pokemon = "ditto"
 
+  useEffect(() => {
+    console.log("Use Effect")
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+     .then((res) => res.json())
+     .then((data) => setPokemonData(data))
+  }, [pokemon])
 
   return (
     <div className="App">
